32a1c8db67f5d2b18068b8475fdcf5e3eacb6df4
Shradha Acharya <shraddhaacharya11o4@gmail.com>
2026-09-17 11:07:33 +0530

    Move Django secret key to environment variable

--- govt/settings.py
+++ govt/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9st*zc27i5(gi^em7wwbh1(16ng@1&blz50+&2p%g2c4epg(lc'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
