7131401cbbfb709298cd73abd43c32bb39ce558e
Shradha Acharya <shraddhaacharya11o4@gmail.com>
2026-09-17 11:13:05 +0530

    Configure Django debug mode using environment variable

--- govt/settings.py
+++ govt/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
